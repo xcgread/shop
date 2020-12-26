1c3d9fe07c97c56098b72dc74ab5629181797d8f
xcg992224 <xcg992224@163.com>
2020-12-26 13:51:23 +0800

    笔记

---
+++ shop/design-learn/src/main/java/com/xuzhihao/design/mode/note.java
@@ -0,0 +1,15 @@
+package com.xuzhihao.design.mode;
+
+/**
+ * 使用单例模式创建JDBC工具类
+ */
+public class note {
+	// 工厂模式
+	// 构建者模式
+	// 代理模式
+	// 责任链模式
+	// 策略模式 优化if else eg:handlerMappings
+	// 观察者模式
+	// 单例模式
+
+}
