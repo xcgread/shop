bfc990fb55cf80fa80fb187fe622b1978cc63672
xcgread <xcg992224@163.com>
2021-01-30 13:26:58 +0800

    add

--- shop/netty-learn/src/main/java/com/xuzhihao/chapter2/client/EchoClientHandler.java
+++ shop/netty-learn/src/main/java/com/xuzhihao/chapter2/client/EchoClientHandler.java
@@ -1,6 +1,7 @@
 package com.xuzhihao.chapter2.client;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
@@ -15,6 +16,11 @@ import io.netty.util.CharsetUtil;
 @Sharable
 //标记该类的实例可以被多个 Channel 共享
 public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+	// 这里使用了SimpleChannelInboundHandler 而不是 ChannelInboundHandlerAdapter
+	// 区别在于 SimpleChannelInboundHandler会主动释放资源（本例中的ByteBuf
+	// in），而ChannelInboundHandlerAdapter不会。
+	// 其实前者是继承自后者，然后复写了channelRead方法，并在channelRead方法中调用了channelRead0方法，然后释放资源（这些资源实现了ReferenceCounted接口）。
+	// channelRead0方法其实是使用了模板方法设计模式。
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) {
 		// 当被通知 Channel是活跃的时候，发送一条消息
@@ -24,7 +30,7 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
 	@Override
 	public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
 		// 记录已接收消息的转储
-		System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+		System.out.println(" Client received : " + ByteBufUtil.hexDump(in.readBytes(in.readableBytes())));
 	}
 
 	@Override
