526c585463f31c5264fdbdc7b06d3c9c856212dc
xcgread <xcg992224@163.com>
2021-02-01 08:58:33 +0800

    up

---
+++ shop/spring-5.3.1/src/main/java/com/xuzhihao/service/A.java
@@ -0,0 +1,22 @@
+package com.xuzhihao.service;
+
+public class A {
+	private B b;
+
+	public void current() {
+		System.out.println("A-" + System.currentTimeMillis());
+	}
+
+	public void test() {
+		b.current();
+	}
+
+	public B getB() {
+		return b;
+	}
+
+	public void setB(B b) {
+		this.b = b;
+	}
+
+}
